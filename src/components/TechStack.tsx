c71c84084934a13c5950050a166ec7bede572c6c
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2023-02-18 21:55:31 +0100

    Added icons for tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,3 +1,27 @@
 export function TechStack() {
-  return <>Tech stack</>;
+  return (
+    <section className="techStackSection">
+      <img src="/images/icons8-html-filetype-48.png" alt="HTML" />
+      <img src="/images/icons8-js-squared.svg" alt="JavaScript" />
+      <img src="/images/icons8-next.js.svg" alt="Next.js" />
+      <img src="/images/icons8-angular-48.png" alt="Angular" />
+      <img src="/images/icons8-react-48.png" alt="React" />
+      <img src="/images/icons8-node-js.svg" alt="Node.js" />
+      <img src="/images/icons8-css-filetype-48.png" alt="CSS" />
+      <img src="/images/icons8-sass.svg" alt="Sass" />
+      <img src="/images/icons8-typescript.svg" alt="TypeScript" />
+      <img src="/images/icons8-mysql-logo.svg" alt="MySQL" />
+      <img
+        src="/images/icons8-mongodb-a-cross-platform-document-oriented-database-program-48.png"
+        alt="MongoDB"
+      />
+      <img
+        src="/images/icons8-web-accessibility-48.png"
+        alt="Web accessibility"
+      />
+      <img src="/images/icons8-github.svg" alt="GitHub" />
+      <img src="/images/icons8-git.svg" alt="Git" />
+      <img src="/images/icons8-figma.svg" alt="Figma" />
+    </section>
+  );
 }
